67a865216ba9ed4922bbdf8f1ff565ef760680f4
Chris Whateley <881698+chriswhateley@users.noreply.github.com>
2025-08-25 20:04:40 -0700

    Forcing node modules to reset

--- node-goat/build.sh
+++ node-goat/build.sh
@@ -6,6 +6,14 @@ export JFROG_BUILD_NUMBER=$(date +%s)
 
 echo "Building and publishing build info for NodeGoat..."
 
+# Clean the npm cache to ensure a fresh download.
+npm cache clean --force
+
+# Remove the node_modules directory and package-lock.json.
+cd ~/jfrog/node-goat
+rm -rf node_modules
+rm -f package-lock.json
+
 # Use JFrog CLI to install npm dependencies and capture build info
 # The --build-name and --build-number flags associate this action with a build
 jf npm install --build-name=$JFROG_BUILD_NAME --build-number=$JFROG_BUILD_NUMBER
