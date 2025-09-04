a5af3003fad4574905b659528bc93d369f7c1d31
Chris Whateley <881698+chriswhateley@users.noreply.github.com>
2025-09-04 14:24:30 -0700

    Streamline for faster builds

--- node-goat/build.sh
+++ node-goat/build.sh
@@ -7,16 +7,16 @@ export JFROG_BUILD_NUMBER=$(date +%s)
 echo "Building and publishing build info for NodeGoat..."
 
 # Clean the npm cache to ensure a fresh download.
-npm cache clean --force
+# npm cache clean --force
 
 # Remove the node_modules directory and package-lock.json just for the lab situation as we pre populated this without the cache to improve performance.
 cd ~/jfrog/node-goat
-rm -rf node_modules
+# rm -rf node_modules
 rm -f package-lock.json
 
 # Use JFrog CLI to install npm dependencies and capture build info
 # The --build-name and --build-number flags associate this action with a build
-jf npm install --build-name=$JFROG_BUILD_NAME --build-number=$JFROG_BUILD_NUMBER --no-fund --no-audit
+jf npm install --build-name=$JFROG_BUILD_NAME --build-number=$JFROG_BUILD_NUMBER --no-fund --no-audit --omit=dev --prefer-offline
 #jf npm ci --no-fund --no-audit (This is what we should be running ina real CI job)
 
 # You can optionally publish the package itself, but for a build scan, the dependencies are what's key
